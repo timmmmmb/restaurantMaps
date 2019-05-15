9432c2c950538bf2989eee49a73643787cbbc45b
Tim Frey <frey.tim@outlook.com>
2019-05-15 16:02:42 +0200

    added chips that you can now add

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,32 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import {Chips} from './chips/chips.component'
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import {HttpClientModule} from '@angular/common/http';
+import {FormsModule, ReactiveFormsModule} from '@angular/forms';
+import {MatNativeDateModule} from '@angular/material';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {DemoMaterialModule} from '../material-module';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    Chips
   ],
   imports: [
+    AppRoutingModule,
     BrowserModule,
-    AppRoutingModule
+    BrowserAnimationsModule,
+    FormsModule,
+    HttpClientModule,
+    DemoMaterialModule,
+    MatNativeDateModule,
+    ReactiveFormsModule,
   ],
-  providers: [],
-  bootstrap: [AppComponent]
+  entryComponents: [AppComponent],
+  bootstrap: [AppComponent],
+  providers: []
 })
 export class AppModule { }
