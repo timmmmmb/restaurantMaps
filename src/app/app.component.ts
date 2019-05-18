77d8801e795a76c2c4e4249aad979e22289deeee
Tim Frey <frey.tim@oublook.com>
2019-05-18 21:19:25 +0200

    added basic leaflet support

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+declare let L;
+import '../../node_modules/leaflet-routing-machine/dist/leaflet-routing-machine.js'
+
 
 @Component({
   selector: 'app-root',
-  templateUrl: 'app.component.html',
-  styles: []
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   title = 'restaurantMaps';
+
+  constructor() {
+
+	}
+  
+  ngOnInit() {
+    const map = L.map('map').setView([51.505, -0.09], 13);
+
+		L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+			attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+		}).addTo(map);
+  }
 }
+
+
